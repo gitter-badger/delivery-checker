ea1f26fd709d067dee3e8885c955bc90866ad1b7
rat <ratulcse27@gmail.com>
2015-04-07 18:05:48 +0600

    add sidebar

--- app/views/includes/sideBar.blade.php
+++ app/views/includes/sideBar.blade.php
@@ -3,15 +3,79 @@
               <!-- sidebar menu start-->
               <ul class="sidebar-menu" id="nav-accordion">
 
-                <!-- subject -->
+                <!-- dashboard -->
 
-                    <li>
+                  <li>
 
-                        <a href="{{ URL::route('dashboard') }}">
-                        <i class="fa fa-laptop"></i>
-                            <span>Dashboard</span>
-                       </a>
-                    </li>
+                      <a href="{{ URL::route('dashboard') }}">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+
+                  {{-- Task Manager --}}
+                  <li>
+
+                      <a href="#">
+                          <i class="fa fa-tasks"></i>
+                          <span>Task Manager</span>
+                      </a>
+                  </li>
+                  {{-- Carrier Accounts --}}
+                  <li>
+
+                      <a href="#">
+                          <i class="fa fa-plane"></i>
+                          <span>Carrier Accounts</span>
+                      </a>
+                  </li>
+
+                  {{-- Shipments --}}
+                  <li>
+
+                      <a href="#">
+                          <i class="fa fa-truck"></i>
+                          <span>Shipments</span>
+                      </a>
+                  </li>
+
+                  {{-- Customers --}}
+                  <li>
+
+                      <a href="#">
+                          <i class="fa fa-user"></i>
+                          <span>Customers</span>
+                      </a>
+                  </li>
+
+                  {{-- Salespersons --}}
+                  <li>
+
+                      <a href="#">
+                          <i class="fa fa-flash"></i>
+                          <span>Sales persons</span>
+                      </a>
+                  </li>
+
+                  {{-- Staff Users --}}
+                  <li>
+
+                      <a href="#">
+                          <i class="fa fa-users"></i>
+                          <span>Staff Users</span>
+                      </a>
+                  </li>
+
+                  {{-- Roles & Permissions --}}
+                  <li>
+
+                      <a href="#">
+                          <i class="fa fa-gears"></i>
+                          <span>Roles & Permissions</span>
+                      </a>
+                  </li>
+
+
                   
 
 
