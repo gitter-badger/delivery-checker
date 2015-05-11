f2a7061037d4cdb67c00ededa4d03c554b0a4e28
rat <ratulcse27@gmail.com>
2015-05-12 02:13:43 +0600

    resolve #21

--- app/views/customers/create.blade.php
+++ app/views/customers/create.blade.php
@@ -21,28 +21,28 @@
                     <div class="form-group">
                         {{ Form::label('name', 'Customer Name*', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('name', '', array('class' => 'form-control', 'placeholder' => 'Customer Name')) }}
+                            {{ Form::text('name', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('email', 'Customer Email*', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::email('email', '', array('class' => 'form-control', 'placeholder' => 'Customer Email')) }}
+                            {{ Form::email('email', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('password', 'Customer Password*', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Customer Password')) }}
+                            {{ Form::password('password', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('company_name', 'Company Name*', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('company_name', '', array('class' => 'form-control', 'placeholder' => 'Company Name')) }}
+                            {{ Form::text('company_name', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
@@ -50,84 +50,84 @@
                     <div class="form-group">
                         {{ Form::label('title', 'Title', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('title', '', array('class' => 'form-control', 'placeholder' => 'Title')) }}
+                            {{ Form::text('title', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('attn_name', 'Attn Name', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('attn_name', '', array('class' => 'form-control', 'placeholder' => 'Attn Name')) }}
+                            {{ Form::text('attn_name', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('address_line_1', 'Address-1', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('address_line_1', '', array('class' => 'form-control', 'placeholder' => 'Address-1')) }}
+                            {{ Form::text('address_line_1', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('address_line_2', 'Address-2', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('address_line_2', '', array('class' => 'form-control', 'placeholder' => 'Address-2')) }}
+                            {{ Form::text('address_line_2', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('city', 'City', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('city', '', array('class' => 'form-control', 'placeholder' => 'City')) }}
+                            {{ Form::text('city', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('state', 'State', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('state', '', array('class' => 'form-control', 'placeholder' => 'State')) }}
+                            {{ Form::text('state', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('zip', 'Zip', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('zip', '', array('class' => 'form-control', 'placeholder' => 'Zip')) }}
+                            {{ Form::text('zip', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('country', 'Country', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('country', '', array('class' => 'form-control', 'placeholder' => 'Country')) }}
+                            {{ Form::text('country', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('telephone', 'Telephone', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('telephone', '', array('class' => 'form-control', 'placeholder' => 'Telephone')) }}
+                            {{ Form::text('telephone', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('fax', 'Fax', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('fax', '', array('class' => 'form-control', 'placeholder' => 'Fax')) }}
+                            {{ Form::text('fax', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('website', 'Website', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('website', '', array('class' => 'form-control', 'placeholder' => 'Website')) }}
+                            {{ Form::text('website', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('fee_percentage', 'Fee Percentage', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('fee_percentage', 50, array('class' => 'form-control', 'placeholder' => 'Fee Percentage')) }}
+                            {{ Form::text('fee_percentage', 50, array('class' => 'form-control')) }}
                         </div>
                     </div>
 
@@ -135,28 +135,28 @@
                     <div class="form-group">
                         {{ Form::label('fee_flat', 'Fee Flat', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('fee_flat', '', array('class' => 'form-control', 'placeholder' => 'Fee Flat')) }}
+                            {{ Form::text('fee_flat', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('sales_id', 'Sales ID', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('sales_id', '', array('class' => 'form-control', 'placeholder' => 'Sales ID')) }}
+                            {{ Form::text('sales_id', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('sales_percentage', 'Sales Percentage', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('sales_percentage', '', array('class' => 'form-control', 'placeholder' => 'Sales Percentage')) }}
+                            {{ Form::text('sales_percentage', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
                     <div class="form-group">
                         {{ Form::label('affiliate_id', 'Affiliate ID', array('class' => 'col-md-2 control-label')) }}
                         <div class="col-md-4">
-                            {{ Form::text('affiliate_id', '', array('class' => 'form-control', 'placeholder' => 'Affiliate ID')) }}
+                            {{ Form::text('affiliate_id', '', array('class' => 'form-control')) }}
                         </div>
                     </div>
 
