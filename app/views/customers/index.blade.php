1f8663a00fb4a2857e2d58e796f8d069391ce088
rat <ratulcse27@gmail.com>
2015-05-21 16:59:20 +0600

    add new column "customer name" resolve #28

--- app/views/customers/index.blade.php
+++ app/views/customers/index.blade.php
@@ -20,6 +20,7 @@
                             <tr>
                                 <th>Customer ID</th>
                                 <th>Customer Name</th>
+                                <th>Company Name</th>
                                 <th>Email</th>
                                 <th>Verified</th>
                                 <th>Enabled</th>
@@ -32,6 +33,7 @@
                                 <tr>
                                     <td>{{ $customer->id }}</td>
                                     <td>{{ $customer->name }}</td>
+                                    <td>{{ $customer->company_name }}</td>
                                     <td>{{ $customer->user->email }}</td>
                                     <td>{{ ($customer->verified == 1) ? 'Yes' : 'No' }}</td>
                                     <td>{{ ($customer->enabled == 1) ? 'Yes' : 'No' }}</td>
